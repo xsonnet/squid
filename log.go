c86efa00456320678b19000c46c57012a8bc3f3f
ZhaoLiang <176405269@qq.com>
2019-06-11 17:03:21 +0800

    判断并创建log文件

--- log.go
+++ log.go
@@ -13,8 +13,8 @@ func SetPath(p string) {
 }
 
 func Log(format string, arg ...interface{}) {
-	result := time.Now().Format("\n[2006-01-02 15:04:05] ")
-	result += fmt.Sprintf(format, arg...)
+	result := time.Now().Format("[2006-01-02 15:04:05] ")
+	result += fmt.Sprintf(format + "\n", arg...)
 	fmt.Print(result)
 	if !Exists(path) {
 		file, err := os.Create(path)
